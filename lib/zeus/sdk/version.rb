ba1932c324f9b02189475a25190ba6e08f464f99
Eric Campbell <ericcampbell59@gmail.com>
2021-02-21 17:23:04 -0600

    bump version

--- lib/zeus/sdk/version.rb
+++ lib/zeus/sdk/version.rb
@@ -1,6 +1,6 @@
 module Zeus
   module Sdk
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
     ENVIRONMENT = ENV['RACK_ENV'] || ENV['RAILS_ENV']
     IS_PRODUCTION = ENVIRONMENT == "production"
   end
