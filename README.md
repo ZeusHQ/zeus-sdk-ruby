dd5a0f81e1aa970baa9b4a36956f8e7b2559cdef
Eric Campbell <ericcampbell59@gmail.com>
2021-02-21 14:57:42 -0600

    initial auth

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ TODO: Delete this and the text above, and describe your gem
 Add this line to your application's Gemfile:
 
 ```ruby
-gem 'zeus-sdk'
+gem 'zeus-sdk-ruby'
 ```
 
 And then execute:
--- lib/zeus/sdk/v1/clients/auth.rb
+++ lib/zeus/sdk/v1/clients/auth.rb
@@ -11,7 +11,17 @@ module Zeus
                     include HTTParty
                     include ServiceBase
 
-                    
+                    def list_users(query)
+                        self.class.get("/api/v1/users", query: query, headers: self.get_headers)
+                    end
+
+                    def signup_with_email_password(user)
+                        self.class.post("/api/v1/users", body: {user: user}, headers: self.get_headers)
+                    end
+
+                    def get_user(id)
+                        self.class.get("/api/v1/users/#{id}", headers: self.get_headers)
+                    end
                 end
             end
         end
